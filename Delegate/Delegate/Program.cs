0599635b3d79ece94e7a04558a4a3a4d750e5391
Orkhansha <orkhansha@code.edu.az>
2022-07-13 02:10:02 +0400

    Added delegates files

---
+++ Delegate/Delegate/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Delegate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            List<string> names = new List<string>() { "Orxan", "Sabir","Cavid", "Emil", "Seid"};
+            Console.WriteLine(GetCheckNames(CheckName, names));
+      
+        }
+
+        public static bool CheckName(string str)
+        {
+            return str != "Cavid";
+        }
+
+        public static string GetCheckNames(Predicate<string> predicate, List<string> names)
+        {
+            string result = null;
+            foreach (var item in names)
+            {
+                if (predicate(item))
+                {
+                    result += (item+" ");
+                }
+            }
+            return result;
+        }
+    }
+
+}
